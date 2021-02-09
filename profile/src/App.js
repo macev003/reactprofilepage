66d86d6941cc37f817bc4b73335f642bdb6f9959
Maira Acevedo <m.acevedo53@yahoo.com>
2021-02-08 17:23:50 -0800

    updated app and index.js

--- profile/src/App.js
+++ profile/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Nav from "./components/Nav";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import Portfolio from "./components/Portfolio";
 
-function App() {
+function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Nav />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Contact" component={Contact} />
+        <Route exact path="/Portfolio" component={Portfolio} />
+      </div>
+    </BrowserRouter>
   );
 }
 
